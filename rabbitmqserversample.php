d06ac73c17b54b1e231878d1efc3de0c02ffdec6
MattToegel <matthew.toegel@njit.edu>
2019-09-15 16:07:13 -0400

    more adjusting

--- rabbitmqserversample.php
+++ rabbitmqserversample.php
@@ -1,10 +1,36 @@
-require_once  __DIR__ . '/vendor/autoload.php';
+<?php
 
-use PhpAmqpLib\Connection\AMQPStreamConnection;
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
 
-$connection = new AMQPStreamConnection('localhost', 5672,'guest','guest');
-$channel = $connection->channel();
+function login($user,$pass){
+	//TODO validate user credentials
+	return true;
+}
 
-$channel->queue_declare('hello', false, false, false, false);
+function request_processor($req){
+	echo "Received Request".PHP_EOL;
+	echo "<pre>" . var_dump($req) . "</pre>";
+	if(!isset($req['type'])){
+		return "Error: unsupported message type";
+	}
+	//Handle message type
+	$type = $req['type'];
+	switch($type){
+		case "login":
+			return login($req['username'], $req['password']);
+		case "validate_session":
+			return validate($req['session_id']);
+	}
+	return array("return_code" => '0',
+		"message" => "Server received request and processed it");
+}
 
+$server = new rabbitMQServer("testRabbitMQ.ini", "sampleServer");
 
+echo "Rabbit MQ Server Start" . PHP_EOL;
+$server->process_requests('request_processor');
+echo "Rabbit MQ Server Stop" . PHP_EOL;
+exit();
+?>
