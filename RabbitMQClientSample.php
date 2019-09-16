3badf2c766947be5aa43c8b9893f0a2276757720
MattToegel <matthew.toegel@njit.edu>
2019-09-16 01:32:11 +0000

    almost working

---
+++ RabbitMQClientSample.php
@@ -0,0 +1,21 @@
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+$client = new RabbitMQClient('testRabbitMQ.ini', 'testServer');
+if(isset($argv[1])){
+	$msg = $argv[1];
+}
+else{
+	$msg = "test message";
+}
+
+$response = $client->send_request($msg);
+
+echo "client received response: " . PHP_EOL;
+print_r($response);
+echo "\n\n";
+
+if(isset($argv[0]))
+echo $argv[0] . " END".PHP_EOL;
