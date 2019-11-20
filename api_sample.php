524ac9023ee99e5cd573cb0c4adbd22424e01376
MattToegel <matthew.toegel@njit.edu>
2019-11-19 19:05:14 -0500

    Sample API using curl, basid on rapid api, but targetting news api directly

---
+++ api_sample.php
@@ -0,0 +1,38 @@
+<?php
+require("config.inc");
+$source = "bitcoin";
+if(isset($argv[1])){
+	//$argv[0] is name of script always
+	$source = $argv[1];
+}
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+	CURLOPT_URL => "https://newsapi.org/v2/everything?q=$source&apiKey=$api_key",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	//CURLOPT_POSTFIELDS => "apiKey=$api_key&newsSource=$source",
+	CURLOPT_HTTPHEADER => array(
+		"content-type: application/x-www-form-urlencoded",
+		//"x-rapidapi-host: $rapid_api_host",
+		//"x-rapidapi-key: $rapid_api_key"
+	),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+	echo "cURL Error #:" . $err;
+} else {
+	//echo $response;
+	echo json_encode($response, JSON_PRETTY_PRINT);
+}
+?>
