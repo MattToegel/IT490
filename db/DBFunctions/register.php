20e0d28c3d1062fe72a26fddba19f74435d884df
MattToegel <Matthew.toegel@njit.edu>
2020-07-13 13:40:43 -0400

    Update register.php

--- db/DBFunctions/register.php
+++ db/DBFunctions/register.php
@@ -3,8 +3,7 @@ function register($username, $password){
 	//from dbconnection.php
 	$stmt = getDB()->prepare("gotta do this one yourself");
 	//don't forget the password_hash($password, PASSWORD_BCRYPT)
-	$stmt->execute([/*fill in the proper mappings*/]);
-	$result = $stmt->fetch(PDO::FETCH_ASSOC);
+	$result = $stmt->execute([/*fill in the proper mappings*/]);
 	//TODO do proper checking, maybe user doesn't exist
 	if($result){
 		return array("status"=>200, "message"=>"Did we register successfully?");
