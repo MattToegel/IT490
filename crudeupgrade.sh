001696c0fbdf202675ed547d72a6ab66b77334c3
MattToegel <Matthew.toegel@njit.edu>
2020-07-13 13:53:25 -0400

    Update crudeupgrade.sh

--- crudeupgrade.sh
+++ crudeupgrade.sh
@@ -1,6 +1,8 @@
 file=$1
 #pass in filename as argument and do our hard coded upgrade process                                                                                                                                                                          cp ./live/$file ./backup/$file.$(date '+%Y%m%d')
 #todo check if file was backed up correctly before delete (hence the cp vs mv)
+#todo append date or increment so we don't overwrite any other backups with the same name
+cp ./live/$file ./backup/$file
 echo "Backed up live file" + $file
 rm ./live/$file
 echo "Deleted live file"
