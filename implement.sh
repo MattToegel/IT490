d7bbcaa0a51d8e76a0a55a1252cf19a287c92592
MattToegel <Matthew.toegel@njit.edu>
2020-07-13 14:21:50 -0400

    Create implement.sh

---
+++ implement.sh
@@ -0,0 +1,19 @@
+
+source=$1
+destination=$2
+
+for filename in $source/*; do
+        base=$(basename "$filename")
+        echo $(basename "$filename")
+        # note, migrations within the same minute will overwrite the backups, be careful
+        cp -r $destination/$base ./backup/$base.$(date "+%Y%m%d_+%H-%M")
+        echo backed up $base
+        #TODO verify file backed up
+        rm -rf $destination/$base
+        echo deleted $base
+        cp -r $filename $destination
+        # maybe it's a good idea to remove it from receive once it has been implemented
+        rm -rf $filename
+        echo copied $filename to "$destination/$base"
+
+done
