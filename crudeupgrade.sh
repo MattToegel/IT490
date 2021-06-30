4bb4b7a1d63550ac3d84475c80d341b19578eb2c
MattToegel <matthew.toegel@njit.edu>
2021-06-30 19:07:08 +0000

    fixed some copy/paste issues

--- crudeupgrade.sh
+++ crudeupgrade.sh
@@ -1,12 +1,13 @@
 file=$1
-#pass in filename as argument and do our hard coded upgrade process                                                                                                                                                                          cp ./live/$file ./backup/$file.$(date '+%Y%m%d')
+#pass in filename as argument and do our hard coded upgrade process
 #todo check if file was backed up correctly before delete (hence the cp vs mv)
 #todo append date or increment so we don't overwrite any other backups with the same name
-cp ./live/$file ./backup/$file
+cp ./dest/$file ./backup/$file.$(date "+%Y%m%d_+%H-%M")
 echo "Backed up live file" + $file
-rm ./live/$file
+rm ./dest/$file
 echo "Deleted live file"
-cp ./preprocess/$file ./live/$file
+cp ./source/$file ./dest/$file
 echo "Copied preprocess file to live"
+rm ./source/$file
 echo "done"
 exit 0
