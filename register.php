2fac10b071f60350df0522f70ef733cb31a1cf4c
Smit Joshi <saj39@njit.edu>
2022-06-18 13:02:51 +0000

    Updating registration page

--- register.php
+++ register.php
@@ -4,7 +4,7 @@
 <head>
     <title>Registration</title>
     <meta name="description" content="test">
-    <meta name=" viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="static/css/style.css">
 </head>
