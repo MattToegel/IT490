8fcfeb9e43060d168528c514e3f141ff0e3b04ee
EpicDepart1337 <cmm66@njit.edu>
2022-06-20 03:29:33 +0000

    updated php files for registery

--- RabbitMQServerSample.php
+++ RabbitMQServerSample.php
@@ -9,6 +9,20 @@ function login($user,$pass){
 	return true;
 }
 
+function register($user,$pass){
+//write user to usernames.txt, write passwords to passwords.txt
+
+
+$userfile = fopen("usernames.txt","w"); $passfile = fopen("passwords.txt","w");
+if(strpos(file_get_contents($userfile), $user) !== false) return false;
+
+fwrite($userfile, $user);
+fwrite($passfile, $pass);
+fclose($userfile);
+fclose($passfile);
+return true;
+
+}
 function request_processor($req){
 	echo "Received Request".PHP_EOL;
 	echo "<pre>" . var_dump($req) . "</pre>";
@@ -24,6 +38,14 @@ function request_processor($req){
 			return validate($req['session_id']);
 		case "echo":
 			return array("return_code"=>'0', "message"=>"Echo: " .$req["message"]);
+		case "register":
+			if(register($req['username'], $req['password']) {
+			return array("return_code"=>'0', "message"=>"Server received userdata and stored it");
+			}
+			else
+			{
+			return array("return_code"=>'0', "message"=>"Server received userdata but either username already exists or userdata invalid");
+			}
 	}
 	return array("return_code" => '0',
 		"message" => "Server received request and processed it");
