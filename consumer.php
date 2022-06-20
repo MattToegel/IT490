df1eded54e44842a22ba073aeda1d9aaf8c1f66b
Smit Joshi <67155481+Indiedude78@users.noreply.github.com>
2022-06-19 20:04:54 -0400

    Revert "testing db with rmq"

--- consumer.php
+++ consumer.php
@@ -1,14 +1,13 @@
 <?php
-	require_once(__DIR__ . "lib/configrmq.php");
     require_once(__DIR__ . "/vendor/autoload.php");
-
+    //include(__DIR__ . "/config_rmq.php");
     use PhpAmqpLib\Connection\AMQPStreamConnection;
     //use PhpAmqpLib\Exchange\AMQPExchangeType;
     require_once(__DIR__ . "/lib/helpers.php");
 
 	//$CONSUMER_TAG = 'reg_consumer';
 
-	$connection = new AMQPStreamConnection($brokerhost, $brokerport, $brokeruser, $brokerpass);
+	$connection = new AMQPStreamConnection('192.168.1.105', 5672, 'smit', 'P@78word');
 
 	$channel = $connection->channel();
 	$channel->queue_declare('reg_queue', false, false, false, false);
@@ -18,24 +17,10 @@
 
 	$callback = function($message) {
         echo "Message received... \n";
-		//echo $message->body;
+		echo $message->body;
 		echo "\n------\n";
-		echo json_decode($message);
-        echo "**Adding record to DB\n";
+        echo "**Sending message back\n";
 		//$message->delivery_info['channel']->basic_ack($message->delivery_info['delivery_tag']);
-		/* $db = getDB();
-		$query = "INSERT INTO Users(fname, lname, email, username, bday, is_active, `password`) ";
-		$query .= "VALUES(:fname, :lname, :email, :username, :bday, :is_active, :password)";
-		$stmt = $db->prepare($query);
-		$params = json_decode($message);
-		$r = $stmt->execute($params);
-		$e = $stmt->errorInfo();
-		if ($e[0] == "00000") {
-			echo "Registration Successful";
-		}
-		else {
-			echo "There was a problem with registration";
-		} */
 	};
 
     //$channel->basic_qos(null, 1, null);
