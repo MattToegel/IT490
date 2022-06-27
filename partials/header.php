e2fb081af2f0fdc55ec3d5aebccb26123c5602ec
Smit Joshi <saj39@njit.edu>
2022-06-27 22:28:06 +0000

    fixing space in header file

--- partials/header.php
+++ partials/header.php
@@ -1,5 +1,5 @@
 <meta name="description" content="test">
-<meta name=" viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" href="static/css/style.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
